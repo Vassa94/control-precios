b65c269851cead55ab23330fccd6cb4752d47389
Vassa94 <vassallo1994@gmail.com>
2022-12-05 09:10:28 -0300

    Backup borrado accidental

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { TableModule } from 'primeng/table';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { MatIconModule} from '@angular/material/icon';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { DataTablesModule } from 'angular-datatables';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import Swal from 'sweetalert2';
 
@@ -22,6 +18,7 @@ import { ExpImpComponent } from './componentes/exp-imp/exp-imp.component';
 import { ReclamosComponent } from './componentes/reclamos/reclamos.component';
 import { FileUploadComponent } from './componentes/file-upload/file-upload.component';
 import { ProgresoComponent } from './componentes/file-upload/progreso/progreso.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -41,12 +38,10 @@ import { ProgresoComponent } from './componentes/file-upload/progreso/progreso.c
     AppRoutingModule,
     HttpClientModule,
     FontAwesomeModule,
-    TableModule,
-    MatIconModule,
-    MatProgressBarModule,
-    DataTablesModule,
     ReactiveFormsModule,
     FormsModule,
+    NgbModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componentes/productos/productos.component.ts
+++ src/app/componentes/productos/productos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BDService } from 'src/app/services/bd.service';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-productos',
@@ -11,13 +12,31 @@ export class ProductosComponent implements OnInit {
 
   productos:any;
   headers:any;
-  objectKeys: any;
+  headers2:any;
+  index:number = 0;
 
 
-  constructor(private datosSis: BDService) { 
-    this.objectKeys = Object.keys;
+  constructor(private datosSis: BDService, private modalService: NgbModal,) { 
+    
 
   }
+
+  fichaProducto = new FormGroup({
+    identificador: new FormControl(),
+    codigo: new FormControl(),
+    marca: new FormControl(),
+    codFabrica: new FormControl(),
+    nombre: new FormControl(),
+    cant: new FormControl(),
+    precioAct: new FormControl(),
+    peso: new FormControl(),
+    alto: new FormControl(),
+    ancho: new FormControl(),
+    prof: new FormControl(),
+    stock: new FormControl(),
+    mostrar: new FormControl(),
+    tags: new FormControl()
+  });
 
   ngOnInit(): void {
     this.getProductos();
@@ -30,14 +49,33 @@ export class ProductosComponent implements OnInit {
     this.datosSis.obtenerDatos().subscribe((data) => {
         this.productos = data;
         this.headers = ["Codigo Oxi","Nombre","Marca","Cod. Fabrica","Cant.","Precio actual","Stock"]
-        //
+        this.headers2 = ["codigoOxi","nombre","marca","codFabrica","cant","precioAct","stock"]
         
    
       });
     }
 
-    view(cont){
-
+    view(cont,i){
+      this.index = i;
+      /*this.fichaProducto.setValue({
+        identificador: this.productos[i].identificador,
+        codigo: this.productos[i].identificador,
+        marca: this.productos[i].identificador,
+        codFabrica: this.productos[i].identificador,
+        nombre: this.productos[i].identificador,
+        cant: this.productos[i].identificador,
+        precioAct: this.productos[i].identificador,
+        peso: this.productos[i].identificador,
+        alto: this.productos[i].identificador,
+        ancho: this.productos[i].identificador,
+        prof: this.productos[i].identificador,
+        stock: this.productos[i].identificador,
+        mostrar: this.productos[i].identificador,
+        tags: this.productos[i].identificador
+      });*/
+      console.log(this.productos[this.index]);
+      
+      this.modalService.open(cont, { centered: true , size: 'lg'});
     }
   
 
