8ae4d16cc3ed103abc368084605f5968724fee1c
Vassa94 <vassallo1994@gmail.com>
2022-12-27 18:30:53 -0300

    Cambios menores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import Swal from 'sweetalert2';
 
 
@@ -18,7 +19,7 @@ import { ExpImpComponent } from './componentes/exp-imp/exp-imp.component';
 import { ReclamosComponent } from './componentes/reclamos/reclamos.component';
 import { FileUploadComponent } from './componentes/file-upload/file-upload.component';
 import { ProgresoComponent } from './componentes/file-upload/progreso/progreso.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FilterPipe } from './pipe/filter.pipe';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     ExpImpComponent,
     ReclamosComponent,
     FileUploadComponent,
-    ProgresoComponent
+    ProgresoComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
