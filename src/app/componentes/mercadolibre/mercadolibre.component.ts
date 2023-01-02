d9cb3164885419d86a32a19e57429f00776f76c1
Vassa94 <vassallo1994@gmail.com>
2023-01-02 15:15:51 -0300

    Agregada seccion mercadolibre

---
+++ src/app/componentes/mercadolibre/mercadolibre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mercadolibre',
+  templateUrl: './mercadolibre.component.html',
+  styleUrls: ['./mercadolibre.component.css']
+})
+export class MercadolibreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
