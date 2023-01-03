a863552f47652c7d97df9135b647a104d56eaf33
Vassa94 <vassallo1994@gmail.com>
2023-01-03 15:18:35 -0300

    cambios menores

--- src/app/componentes/web/web.component.ts
+++ src/app/componentes/web/web.component.ts
@@ -79,31 +79,6 @@ export class WebComponent implements OnInit {
 
 
 
-  filtrarTabla($event) {
-    // Obtiene el término de búsqueda del cuadro de búsqueda
-    let busqueda = $event.target.value.toLowerCase();
-
-    // Itera sobre cada fila de la tabla
-    let filas = Array.from(document.querySelectorAll("tbody tr"));
-    for (let fila of filas) {
-      let celdas = Array.from(fila.querySelectorAll("td"));
-      let coincidencias = 0;
-
-      // Compara el valor de cada celda con el término de búsqueda
-      for (let celda of celdas) {
-        if (celda.innerHTML.toLowerCase().includes(busqueda)) {
-          coincidencias++;
-        }
-      }
-
-      // Muestra la fila si hay al menos una coincidencia
-      let row: HTMLTableRowElement = fila;
-      if (coincidencias > 0) {
-        row.style.display = "";
-      } else {
-        row.style.display = "none";
-      }
-    }
-  }
+  
 }
 
