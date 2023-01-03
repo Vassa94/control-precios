4e58c8e6d315c1d047566806faaf9947ece3d4ec
Vassa94 <vassallo1994@gmail.com>
2023-01-03 14:58:38 -0300

    cambios en el estilo de bara en tabla

--- src/app/componentes/web/web.component.ts
+++ src/app/componentes/web/web.component.ts
@@ -11,92 +11,99 @@ import { TruefalseComponent } from '../truefalse/truefalse.component';
   styleUrls: ['./web.component.css']
 })
 export class WebComponent implements OnInit {
-  web:any;
+  web: any;
   headers: any;
-  headers2:any;
+  headers2: any;
   search: string = '';
   cargando: boolean = true;
 
-  constructor(private datosSis: BDService, private modalService: NgbModal,private formModule:NgbModule) { }
+  constructor(private datosSis: BDService, private modalService: NgbModal, private formModule: NgbModule) { }
 
   ngOnInit(): void {
     this.getProductos();
   }
 
-  getProductos():void {
+  getProductos(): void {
     this.datosSis.obtenerWeb().toPromise().then((data) => {
-        this.web = data;
-        this.headers = ["URL",
-                        "Nombre",
-                        "Categorias",
-                        "Precio",
-                        "Precio Promocional",
-                        "Peso",
-                        "Alto (cm)",
-                        "Ancho (cm)",
-                        "Profundidad (cm)",
-                        "Stock",
-                        "Codigo",
-                        "Mostrar en tienda",
-                        "Envio Gratis",
-                        "Tags",
-                        "Marca"];
-        this.headers2 = ["url",
-                        "nombre",
-                        "categorias",
-                        "precio",
-                        "precioProm",
-                        "peso",
-                        "alto",
-                        "ancho",
-                        "profundidad",
-                        "stock",
-                        "codigo",
-                        "mostrar",
-                        "envio",
-                        "tags",
-                        "marca"];
+      this.web = data;
+      this.headers = ["Nombre",
+        "Categorias",
+        "Precio",
+        "Oferta",
+        "Peso",
+        "Alto",
+        "Ancho",
+        "Prof.",
+        "Stock",
+        "Codigo",
+        "Mostrar",
+        "Envio g.",
+        "Tags",
+        "Marca"];
+      this.headers2 = ["nombre",
+        "categorias",
+        "precio",
+        "precioProm",
+        "peso",
+        "alto",
+        "ancho",
+        "profundidad",
+        "stock",
+        "codigo",
+        "mostrar",
+        "envio",
+        "tags",
+        "marca"];
+      console.log(this.headers2);
 
-        this.cargando = false;
-      
-      });
-      
-      
-    }
+      this.cargando = false;
 
-  searchProduct = (text$: Observable<string>) =>
-    text$.pipe(
-      
-      debounceTime(200),
-      map((term: string) => term === '' ? []
-        : this.web.filter(v => v.nombre.toLowerCase().includes(term.toLowerCase()) ||
-                                    v.marca.toLowerCase().includes(term.toLowerCase()) ||
-                                    v.codigo.toString().toLowerCase().includes(term.toLowerCase()) ||
-                                    v.tags.toString().toLowerCase().includes(term.toLowerCase()))
-                                    .slice(0, 10))
-    )
+    });
 
-    sort(event) {
-      this.web.sort((a, b) => {
-        if (a[event.column] < b[event.column]) {
-          return event.order === 'asc' ? -1 : 1;
-        } else if (a[event.column] > b[event.column]) {
-          return event.order === 'asc' ? 1 : -1;
+
+  }
+
+
+
+  view(cont, row) {
+    /* this.web.setValue({
+      codigo: row.codigo,
+      marca: row.marca,
+      cod_Fabrica: row.cod_Fabrica,
+      descripcion: row.descripcion,
+      stock: row.stock,
+      precioPublico: row.precioPublico,
+    }) */
+    this.modalService.open(cont, { centered: true });
+  }
+
+
+
+  filtrarTabla($event) {
+    // Obtiene el término de búsqueda del cuadro de búsqueda
+    let busqueda = $event.target.value.toLowerCase();
+
+    // Itera sobre cada fila de la tabla
+    let filas = Array.from(document.querySelectorAll("tbody tr"));
+    for (let fila of filas) {
+      let celdas = Array.from(fila.querySelectorAll("td"));
+      let coincidencias = 0;
+
+      // Compara el valor de cada celda con el término de búsqueda
+      for (let celda of celdas) {
+        if (celda.innerHTML.toLowerCase().includes(busqueda)) {
+          coincidencias++;
         }
-        return 0;
-      });
-    }
+      }
 
-    view(cont,row){
-      /* this.web.setValue({
-        codigo: row.codigo,
-        marca: row.marca,
-        cod_Fabrica: row.cod_Fabrica,
-        descripcion: row.descripcion,
-        stock: row.stock,
-        precioPublico: row.precioPublico,
-      }) */
-      this.modalService.open(cont, { centered: true });
+      // Muestra la fila si hay al menos una coincidencia
+      let row: HTMLTableRowElement = fila;
+      if (coincidencias > 0) {
+        row.style.display = "";
+      } else {
+        row.style.display = "none";
+      }
     }
-
+  }
 }
+
