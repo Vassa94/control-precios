a48cface5ffbe81f7e1a10d5f3e26c4b0b2a0849
Vassa94 <vassallo1994@gmail.com>
2023-01-05 09:59:43 -0300

    Correccion botones

--- src/app/componentes/mercadolibre/mercadolibre.component.ts
+++ src/app/componentes/mercadolibre/mercadolibre.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BDService } from 'src/app/services/bd.service';
 
 @Component({
   selector: 'app-mercadolibre',
@@ -6,10 +8,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mercadolibre.component.css']
 })
 export class MercadolibreComponent implements OnInit {
+  ml: any;
+  headers: any;
+  headers2: any;
+  search: string = '';
+  cargando: boolean = true;
 
-  constructor() { }
+  constructor(private datosSis: BDService, private modalService: NgbModal, private formModule: NgbModule) { }
 
   ngOnInit(): void {
+    this.obtenerDatos();
+  }
+
+  obtenerDatos(){
+    this.datosSis.obtenerMl().subscribe((data) =>{
+      this.ml = data;
+      this.headers = ["itemid",
+                      "itemsku",
+                      "categoryid",
+                      "listingtypeid",
+                      "title",
+                      "availablequantity",
+                      "price",
+                      "condition",
+                      "status",
+                      "freeshipping",
+                      "full",
+                      "localpickup",
+                      "warrantytime",
+                      "cataloglisting",
+                      "shipping",
+                      "picture1",
+                      "marca",
+                      "modelo",
+                      "peso"]
+      this.headers2 = ["itemid",
+                      "itemsku",
+                      "categoryid",
+                      "listingtypeid",
+                      "title",
+                      "availablequantity",
+                      "price",
+                      "condition",
+                      "status",
+                      "freeshipping",
+                      "full",
+                      "localpickup",
+                      "warrantytime",
+                      "cataloglisting",
+                      "shipping",
+                      "picture1",
+                      "marca",
+                      "modelo",
+                      "peso"]
+      this.cargando = false;
+    });
   }
 
 }
