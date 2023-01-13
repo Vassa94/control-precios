8f679d557f6a4615f96ee7b3f765581c2482841c
Vassa94 <vassallo1994@gmail.com>
2023-01-13 18:19:57 -0300

    Actualizacion masiva precios

--- src/app/componentes/productos/productos.component.ts
+++ src/app/componentes/productos/productos.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { BDService } from 'src/app/services/bd.service';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormControl, FormGroup, NgForm, Validators, NgModel } from '@angular/forms';
-import {NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 import { debounceTime, map, Observable, Subject, Subscription } from 'rxjs';
 import { FilterPipe } from 'src/app/pipe/filter.pipe';
 import * as Papa from 'papaparse';
 import Swal from 'sweetalert2';
+import * as FileSaver from 'file-saver';
+
 
 
 @Component({
@@ -17,34 +19,35 @@ import Swal from 'sweetalert2';
 })
 export class ProductosComponent implements OnInit {
 
-  productos:any;
-  headers:any;
-  headers2:any;
+  productos: any;
+  headers: any;
+  headers2: any;
   search: string;
-  filteredProductos: Record<string, any>[]  = [];
+  filteredProductos: Record<string, any>[] = [];
   cargando: boolean = true;
   actualizador: any;
   csvFile: any;
-  
+  reader = new FileReader();
 
 
-  constructor(private datosSis: BDService, private modalService: NgbModal,private formModule:NgbModule) { 
+
+  constructor(private datosSis: BDService, private modalService: NgbModal, private formModule: NgbModule) {
     this.search = '';
 
   }
 
-  
 
-  producto= new FormGroup ({
-    codigo: new FormControl('',[Validators.required, Validators.min(111111) ,Validators.max(999999)]),
-    cod_Fabrica: new FormControl('',Validators.required),
-    descripcion: new FormControl('',[Validators.required, Validators.maxLength(60)]),
-    marca: new FormControl('',Validators.required),
-    precioPublico: new FormControl('',Validators.required),
-    stock: new FormControl('',Validators.required)
+
+  producto = new FormGroup({
+    codigo: new FormControl('', [Validators.required, Validators.min(111111), Validators.max(999999)]),
+    cod_Fabrica: new FormControl('', Validators.required),
+    descripcion: new FormControl('', [Validators.required, Validators.maxLength(60)]),
+    marca: new FormControl('', Validators.required),
+    precioPublico: new FormControl('', Validators.required),
+    stock: new FormControl('', Validators.required)
   })
 
-  file= new FormGroup({
+  file = new FormGroup({
     csv: new FormControl('')
   })
 
@@ -54,150 +57,171 @@ export class ProductosComponent implements OnInit {
     this.searchProduct(search$).subscribe(productos => this.filteredProductos = productos);
 
   }
- 
+
   /* Obtener los datos de la base de datos y almacenarlos en la variable productos. */
-  getProductos():void {
+  getProductos(): void {
     this.datosSis.obtenerDatos().subscribe((data) => {
-        this.productos = data;
-        this.headers = ["Codigo Oxi","Nombre","Marca","Cod. Fabrica","Precio actual","Stock"];
-        this.headers2 = ["codigo","descripcion","marca","cod_Fabrica","precioPublico","stock"];
-        this.cargando = false;
+      this.productos = data;
+      this.headers = ["Codigo Oxi", "Nombre", "Marca", "Cod. Fabrica", "Precio actual", "Stock"];
+      this.headers2 = ["codigo", "descripcion", "marca", "cod_Fabrica", "precioPublico", "stock"];
+      this.cargando = false;
+    });
+  }
+
+  view(cont, row) {
+    this.producto.setValue({
+      codigo: row.codigo,
+      marca: row.marca,
+      cod_Fabrica: row.cod_Fabrica,
+      descripcion: row.descripcion,
+      stock: row.stock,
+      precioPublico: row.precioPublico,
+    })
+    this.modalService.open(cont, { centered: true });
+  }
+
+  new() {
+    this.producto.setValue({
+      codigo: " ",
+      marca: " ",
+      cod_Fabrica: " ",
+      descripcion: " ",
+      stock: " ",
+      precioPublico: " ",
+    })
+  }
+
+  parser(event: any) {
+    let file: File = event.target.files[0];
+    if ( file ){
+    this.reader.readAsText(file);
+    this.reader.onload = (event: any) => {
+      let content = event.target.result;
+      Papa.parse(content, {
+        header: true,
+        complete: (results) => {
+          let data = results.data;
+          this.actualizarPrecios(data);
+          //console.log(data);
+          Swal.fire({
+            title: '¡Genial!',
+            text: 'Precios actualizados',
+            icon: 'success',
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          
+        }
       });
     }
-
-    /**
-     * La función toma dos parámetros, el primero es el id del modal y el segundo es la fila de la
-     * tabla
-     * @param cont - es el id del modal
-     * @param row - es la fila que se está editando.
-     */
-    view(cont,row){
-      this.producto.setValue({
-        codigo: row.codigo,
-        marca: row.marca,
-        cod_Fabrica: row.cod_Fabrica,
-        descripcion: row.descripcion,
-        stock: row.stock,
-        precioPublico: row.precioPublico,
-      })
-      this.modalService.open(cont, { centered: true });
-    }
-
-  /**
-   * La función se llama cuando el usuario hace clic en el botón "Nuevo". Establece los valores del
-   * formulario en cadenas vacías.
-   */
-    new() {
-      this.producto.setValue({
-        codigo: " ",
-        marca: " ",
-        cod_Fabrica: " ",
-        descripcion: " ",
-        stock: " ",
-        precioPublico: " ",
-      })
-    }
-
-  parser(csvContent) {
-    //const csvContent = '1,2,3\n4,5,6';
-    console.log("paso2");
-    const config = {
-      header: true,
-      transformHeader: (header: string) => header.toLowerCase()
-    
-    };
-    console.log("paso3");
-    console.log(csvContent);
-    
-    const result = Papa.parse(csvContent);
-    console.log("paso4");
-    this.actualizador = result.data;
-    console.log(this.actualizador);
-    
+  } else{
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Debes cargar un archivo!',
+      footer: '<a href="">Why do I have this issue?</a>'
+    })
   }
-   
+  console.log("todo correcto");
+  
+  }
+
   
 
-   act(actualizar) {
-      
-        this.modalService.open(actualizar, {centered: true}).result.then((result) => {  
-          console.log("paso1");          
-          this.parser(this.file.value.csv);
-        }, (reason) => {
-          // maneja el motivo por el que se cerró el modal
-        });
-      };
-      
-    /**
-     * Abre una ventana modal.
-     * @param productNew - El nombre del modal que desea abrir.
-     */
-    form(productNew){
-      this.new();
-      this.modalService.open(productNew,{centered: true}) ;
-    }
+  act(actualizar) {
 
-    
-    
+    this.modalService.open(actualizar, { centered: true }).result.then((result) => {
+      this.parser(this.file.value.csv);
+    }, (reason) => {
+      // maneja el motivo por el que se cerró el modal
+    });
+  };
 
-    agregarProducto(){
-      const body= {
-        codigo: this.producto.value.codigo,
-        marca: this.producto.value.marca,
-        cod_Fabrica: this.producto.value.cod_Fabrica,
-        descripcion: this.producto.value.descripcion,
-        stock: this.producto.value.stock,
-        precioPublico:this.producto.value.precioPublico
-      };
-      console.table(body)
-      this.datosSis.crearProducto(body).subscribe((data) => {
-        Swal.fire({
-          title: '¡Genial!',
-          text: 'Producto Agregado',
-          icon: 'success',
-          showConfirmButton: false,
-          timer: 1500,
-        });
-      });
+  form(productNew) {
+    this.new();
+    this.modalService.open(productNew, { centered: true });
+  }
 
-      /* Swal.fire({
+  agregarProducto() {
+    const body = {
+      codigo: this.producto.value.codigo,
+      marca: this.producto.value.marca,
+      cod_Fabrica: this.producto.value.cod_Fabrica,
+      descripcion: this.producto.value.descripcion,
+      stock: this.producto.value.stock,
+      precioPublico: this.producto.value.precioPublico
+    };
+    console.table(body)
+    this.datosSis.crearProducto(body).subscribe((data) => {
+      Swal.fire({
         title: '¡Genial!',
         text: 'Producto Agregado',
         icon: 'success',
         showConfirmButton: false,
         timer: 1500,
-      }); */
-      
-    }
+      });
+    });
 
-    borrarProducto(id){
-       this.datosSis.borrarProducto(id).subscribe((data)=>{})
-       for (let i=0; i<this.productos.length; i++) {
-        if (this.productos[i].codigo === id) {
-          this.productos.splice(i,1);
-        }
+    /* Swal.fire({
+      title: '¡Genial!',
+      text: 'Producto Agregado',
+      icon: 'success',
+      showConfirmButton: false,
+      timer: 1500,
+    }); */
+
+  }
+
+  borrarProducto(id) {
+    this.datosSis.borrarProducto(id).subscribe((data) => { })
+    for (let i = 0; i < this.productos.length; i++) {
+      if (this.productos[i].codigo === id) {
+        this.productos.splice(i, 1);
+      }
     }
   }
 
-  actualizarProducto(id){
-    
-  }
-  
+  actualizarProducto(id) {
 
-    /* Una función que se llama cuando el usuario escribe en la barra de búsqueda. Filtra los productos
-    por nombre, marca, código o código de fábrica. */
-    searchProduct = (text$: Observable<string>) =>
+  }
+
+  actualizarPrecios(body){
+    this.datosSis.actuProductos(body).subscribe((data) => {      
+    });
+    this.datosSis.actuWeb(body).subscribe((data) => {      
+    });
+  }
+
+  descargarCSV() {
+    let data = this.productos;
+    let dataUTF8 = data.map(element => {
+      for (let key in element) {
+        element[key] = new TextEncoder().encode(element[key]);
+      }
+      return element;
+    });
+    const csvData = Papa.unparse(dataUTF8);
+    let date = new Date().toLocaleString();
+    let fileName = 'productos_' + date + '.csv';
+    let blob = new Blob([csvData], { type: 'text/csv;charset=utf-8' });
+    FileSaver.saveAs(blob, fileName);
+
+  }
+
+
+  /* Una función que se llama cuando el usuario escribe en la barra de búsqueda. Filtra los productos
+  por nombre, marca, código o código de fábrica. */
+  searchProduct = (text$: Observable<string>) =>
     text$.pipe(
-      
+
       debounceTime(200),
       map((term: string) => term === '' ? []
         : this.productos.filter(v => v.descripcion.toLowerCase().includes(term.toLowerCase()) ||
-                                    v.marca.toLowerCase().includes(term.toLowerCase()) ||
-                                    v.codigo.toString().toLowerCase().includes(term.toLowerCase()) ||
-                                    v.cod_Fabrica.toString().toLowerCase().includes(term.toLowerCase()))
-                                    .slice(0, 10))
+          v.marca.toLowerCase().includes(term.toLowerCase()) ||
+          v.codigo.toString().toLowerCase().includes(term.toLowerCase()) ||
+          v.cod_Fabrica.toString().toLowerCase().includes(term.toLowerCase()))
+          .slice(0, 10))
     )
-
 
   /**
    * Si la columna es la misma, no haga nada. Si la columna es diferente, ordene la matriz por columna
@@ -214,6 +238,6 @@ export class ProductosComponent implements OnInit {
       return 0;
     });
   }
-  
+
 
 }
