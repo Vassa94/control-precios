2d6fe7a5d17ca66e0836c5c245e3862635e57a2d
Vassa94 <vassallo1994@gmail.com>
2023-02-02 16:08:22 -0300

    Conexión API Ml

---
+++ src/app/services/ml-api.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, Subject, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MlApiService {
+
+  constructor(private http: HttpClient) { }
+  API = 'https://api.mercadolibre.com';
+  seller_id = '33953861';
+  token = '';
+  expires_in = 21600;
+
+  getNewToken() {
+    const headers = new HttpHeaders({
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    });
+  
+    const refresh_token = localStorage.getItem('refresh_token');
+    const body = `grant_type=refresh_token&client_id=2720596362974064&client_secret=5B2NxORFWnFHyCk1zQ1h3qHAFqjc4zED&refresh_token=${refresh_token}`;
+  
+    this.http.post('https://api.mercadolibre.com/oauth/token', body, { headers })
+      .subscribe(response => {
+        const access_token = response['access_token'];
+        const expires_in = response['expires_in'];
+        const new_refresh_token = response['refresh_token'];
+        // Guarde el nuevo token, su tiempo de expiración y el nuevo refresh token en su aplicación
+      });
+  }
+
+
+
+}
