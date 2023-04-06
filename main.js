c72f7c68afa218b1e54cebc5a2f13246740b5772
Vassa94 <vassallo1994@gmail.com>
2023-04-06 09:38:12 -0300

    .

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ createWindow = () => {
         width: 1366,
         height: 768,
         title: "Control de precios",
-        titleBarStyle: 'hidden',
+        //titleBarStyle: 'hidden',
         titleBarOverlay: {
             color: '#212529',
             symbolColor: '#ffff',
